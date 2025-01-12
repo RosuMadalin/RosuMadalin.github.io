e9dbf49a4a2c2613d2e525bfaf8980b8eab1567c
RosuMadalin <rosu.madalinul@yahoo.com>
2025-01-12 16:08:19 +0200

    ultmul care merge

---
+++ index.js
@@ -0,0 +1,10 @@
+// // Autentificare firebase pentru files
+// firebase.auth().signInAnonymously()
+//   .then(() => {
+//     console.log("Signed in anonymously");
+//   })
+//   .catch((error) => {
+//     console.error("Error during anonymous sign-in:", error);
+//   });
+
+  
